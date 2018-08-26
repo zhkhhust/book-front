a210e87188c12623f6c4a9282c3dc1e6a3d9d807
郑凯华 <zhengkaihua@zhengkaihuadeMacBook-Pro.local>
2018-08-27 07:58:24 +0800

    add eslint

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -3,13 +3,13 @@ import Identicon from 'identicon.js'
 import etmJS from 'etm-js'
 // 存储异步操作,获取数据用commit操作mutation
 // let baseUrl = 'http://127.0.0.1:4096/api/dapps/1a351d2092cf3bd9e4289372fccf39e3e7af02d4eef62f266c58b07e02226f1b' // 测试环境
-let baseUrl = 'http://140.143.122.232:4096/api/dapps/f592d77aa08c15e6dfae7062cc71e82ac37185d1d15e9192031ca60fc7c6bc52' // 真实环境
+let baseUrl = 'http://140.143.122.232:4096/api/dapps/ef7f4fca1cb38e8ec3ecf4daa0722b89a560c19b45a994af059b5b8e9a95b3ce' // 真实环境
 // let loginurl = baseUrl + '/login'测试环境下login传输， 有暴露secret可能
 let loginurl = baseUrl + '/accounts/'
 // 测试环境
 // let posturl = baseUrl + '/transactions/unsigned'
 let posturl = baseUrl + '/transactions/signed'
-let articleslisturl = baseUrl + '/articles'
+let articleslisturl = baseUrl + '/books'
 let transRecord = baseUrl + '/transfers'
 
 function stringToHex (str) {
